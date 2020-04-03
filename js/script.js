99f28a57121b1ac462b233ed6b95e789b17d7397
Michał Kopacz <kopaczmichal093@gmail.com>
2020-04-03 21:00:52 +0200

    Add script for randomNumer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 let playerMove = 'papier';
 let computerMove = 'kamień';
+let randomFraction = Math.random();
+
+printMessage('wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage("Liczba po zaokrągleniu w dół to: " + roundNumber);
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
